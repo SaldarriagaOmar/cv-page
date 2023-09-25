479d184170a3a1c44e07f07fbe38cdf346144e06
Ómar Saldarriaga <saldarriagaomar@gmail.com>
2023-09-24 21:11:39 -0500

    Cuenta actualizada

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jrumwklzqgjmialglypi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpydW13a2x6cWdqbWlhbGdseXBpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDg4Mjk1MSwiZXhwIjoyMDEwNDU4OTUxfQ.g7nZZKHoVtU5nOfnQ2wHYoxDJ0pkmhxp54OnAiW4JFM';
+const supabaseUrl = 'https://xsmjbyboucylbbnizgko.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzbWpieWJvdWN5bGJibml6Z2tvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NTYwNzA3OSwiZXhwIjoyMDExMTgzMDc5fQ.V8JyMbPWC4LV26cr8Eki0lkUiFXlf9RIOeUoj30wQ0Q';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
